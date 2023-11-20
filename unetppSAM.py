88adc09a6f08b3f5c9da600b16b2959f7e691031
Terry2999 <terry.hsu321@gmail.com>
2023-11-20 12:32:31 +0800

    unetppSAM.py Add SAM_dice and different mask color

--- unetppSAM.py
+++ unetppSAM.py
@@ -34,23 +34,32 @@ def get_bounding_box(ground_truth_map):
 
     return bbox
 
-def show_mask(mask, ax, random_color=False):
+def show_mask(mask, ax, color=False, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    elif color == "red":
+        color = np.array([255/255, 70/255, 70/255, 0.5])
+    elif color == "green":
+        color = np.array([70/255, 255/255, 70/255, 0.5])
+    elif color == "blue":
+        color = np.array([70/255, 150/255, 255/255, 0.5])
     else:
         color = np.array([30/255, 144/255, 255/255, 0.6])
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
 
+def dice_coefficient(preds, targets):
+        smooth = 1.0
+        assert preds.size() == targets.size()
+
+        iflat = preds.contiguous().view(-1)
+        tflat = targets.contiguous().view(-1)
+        intersection = (iflat * tflat).sum()
+        dice = (2.0 * intersection + smooth) / (iflat.sum() + tflat.sum() + smooth)
+        return dice
+
 def gen_ans(config, is_show_ans = True, is_gen_compare = True, is_show_compare = True, is_unetpp = True):
-    '''
-    由預訓練的模型生成圖片\n
-    config:存在config.json的參數\n
-    is_show_ans:是否在生成完後直接顯示圖片\n
-    is_gen_compare:是否生成對照圖\n
-    is_show_compare:是否在生成對照圖完後直接顯示圖片
-    '''
     print("Generating ans")
     #載入model 架構
     model = SamModel.from_pretrained("facebook/sam-vit-base")
@@ -77,7 +86,7 @@ def gen_ans(config, is_show_ans = True, is_gen_compare = True, is_show_compare =
     ground_truth_mask = np.array(mask)
     prompt = get_bounding_box(ground_truth_mask)
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    # model = model.to('cuda') 
+    # model = model.to('cuda')
     # prepare image + box prompt for the model
     inputs = processor(image, input_boxes=[[prompt]], return_tensors="pt").to(device)
     # for k,v in inputs.items():
@@ -112,36 +121,7 @@ def gen_ans(config, is_show_ans = True, is_gen_compare = True, is_show_compare =
         img = Image.open(output_path)
         img.show()
 
-    if is_gen_compare: #顯示最後結果(原圖、預測、答案)
-        _, axes = plt.subplots(1, 3)
-        # 在子圖 1 中顯示第一張圖片
-        axes[0].imshow(image)
-        axes[0].set_title("origin Image")
-
-        axes[1].imshow(np.array(image))
-        show_mask(medsam_seg, axes[1])
-        axes[1].title.set_text(f"Predicted mask")
-
-        axes[2].imshow(np.array(image))
-        ground_truth_seg = np.array(mask)
-        show_mask(ground_truth_seg, axes[2])
-        axes[2].title.set_text(f"Ground truth mask")
-        
-    #存檔
-        output_folder = config["compare_img_floder"]
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-
-        save_name = os.path.basename(image_path)
-        save_name += config["compare_img_name"]
-        output_path = os.path.join(output_folder, save_name)
-        plt.savefig(output_path)
-        if is_show_compare:
-            img = Image.open(output_path)
-            img.show()
-        plt.close("all")
-
-    if is_unetpp:
+    if is_gen_compare:
         model = smp.UnetPlusPlus(encoder_name="resnet34", encoder_weights=None, in_channels=1, classes=1).to(device)
         model.load_state_dict(torch.load('unetplusplus_chkpt/unetplusplus.pth'))
 
@@ -149,55 +129,63 @@ def gen_ans(config, is_show_ans = True, is_gen_compare = True, is_show_compare =
             transforms.Resize((256, 256)),
             transforms.ToTensor(),
         ])
+        
+        uimage = Image.open(image_path).convert('L')
 
-        def dice_coefficient(preds, targets):
-            smooth = 1.0
-            assert preds.size() == targets.size()
-
-            iflat = preds.contiguous().view(-1)
-            tflat = targets.contiguous().view(-1)
-            intersection = (iflat * tflat).sum()
-            dice = (2.0 * intersection + smooth) / (iflat.sum() + tflat.sum() + smooth)
-            return dice
-
-        image = Image.open(image_path).convert('L')
-        # mask = Image.open(mask_path)
-        # image = image.resize((256, 256))
-        # mask = mask.resize((256, 256))
-
-        image_tensor = preprocess(image).unsqueeze(0).to(device)
+        # print("mask\n", mask)
+        image_tensor = preprocess(uimage).unsqueeze(0).to(device)
         mask_tensor = preprocess(mask).unsqueeze(0).to(device)
+        # print("mask_tensor\n", mask_tensor)
 
         with torch.no_grad():
             model.eval()
             pred_mask = model(image_tensor)
             pred_mask = (torch.sigmoid(pred_mask) > 0.7).int()
-            dice = dice_coefficient(pred_mask,mask_tensor).cpu().item()
+            # dice = dice_coefficient(pred_mask,mask_tensor).cpu().item()
 
-        print("Dice = ", dice)
+        # print("Dice = ", dice)
 
-        # _, axs = plt.subplots(1,3,figsize=(25,5), gridspec_kw={'wspace': 0.3, 'hspace': 0, 'width_ratios': [1, 1, 1]})
-        _, axs = plt.subplots(1, 3)
-        # axs[0].imshow(mask)
-        # axs[0].set_title('Mask')
-        # # axs[0].axis('off')
-        # axs[1].imshow(pred_mask[0, 0].cpu().numpy(), cmap='gray')  # 取第一張圖的第一個通道的內容
-        # axs[1].set_title('Predicted Mask')
-        # axs[1].set_title(f"predict_mask\ndice = {dice :.2f}")
-        # axs[1].axis('off')
+        _, axs = plt.subplots(1, 4)
+        # _, axs = plt.subplots(1, 5)
+
+        # print("SAM pred_mask:\n", medsam_seg)
+        # print("Unet-pp pred_mask:\n", pred_mask)
 
         axs[0].imshow(image)
-        axs[0].set_title('Image')
+        axs[0].set_title("origin")
         axs[0].axis('off')
-        axs[1].imshow(mask)
-        axs[1].set_title('Mask')
+
+        axs[1].imshow(np.array(image))
+        ground_truth_seg = np.array(mask)
+        show_mask(ground_truth_seg, axs[1], "blue")
+        axs[1].title.set_text(f"mask")
         axs[1].axis('off')
-        axs[2].imshow(pred_mask[0, 0].cpu().numpy(), cmap='gray')  # 取第一張圖的第一個通道的內容
-        axs[2].set_title('Predicted Mask')
-        axs[2].set_title(f"predict_mask\ndice = {dice :.2f}")
+
+        axs[2].imshow(np.array(image))
+        show_mask(medsam_seg, axs[2], "green")
+        axs[2].set_title('SAM predict')
+        medsam_seg = [[medsam_seg]]
+        medsam_seg = torch.tensor(medsam_seg)
+        # print("After process SAM pred_mask:\n", medsam_seg)
+        dice = dice_coefficient(medsam_seg, mask_tensor).cpu().item()
+        print("SAM dice = ", dice)
+        axs[2].set_title(f"SAM predict\ndice = {dice :.2f}")
         axs[2].axis('off')
 
-        # plt.show()
+        axs[3].imshow(np.array(image))
+        show_mask(pred_mask[0, 0].cpu().numpy(), axs[3], "red")
+        dice = dice_coefficient(pred_mask,mask_tensor).cpu().item()
+        print("Unet-pp dice = ", dice)
+        axs[3].set_title(f"Unet-pp predict\ndice = {dice :.2f}")
+        axs[3].axis('off')
+
+        # axs[4].imshow(np.array(uimage), alpha=0.3)
+        # ground_truth_seg = np.array(mask)
+        # show_mask(ground_truth_seg, axs[4], "blue")
+        # show_mask(medsam_seg, axs[4], "green")
+        # show_mask(pred_mask[0, 0].cpu().numpy(), axs[4], "red")
+        # axs[4].title.set_text(f"All in one")
+        # axs[4].axis('off')
 
         output_folder = config["unetpp_floder"]
         if not os.path.exists(output_folder):
