4785fb3f12f5b3b6afd312f958301f6db32bcf12
wallence <a33369097@gmail.com>
2023-12-05 18:05:47 +0800

    fix CNN train model

--- wall_cnn.py
+++ wall_cnn.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 
 class resNet(nn.Module):
-    def __init__(self, in_channel=3, out_channel=1,img_width=224, img_heigth=224):# input img 224X224
+    def __init__(self, in_channel=1, out_channel=1,img_width=256):# input img 256X256
         super(resNet, self).__init__() 
-        
-        self.x7conv3to64 = nn.Conv2d(in_channel,64,7,1,1,bias=False)
+        self.out_channel = out_channel
+        self.x7conv3to64 = nn.Conv2d(in_channel,64,7,1,3,bias=False)
         self.x1conv64to64 = nn.Conv2d(64,64,1,1)
         self.x1conv128to64 = nn.Conv2d(128,64,1,1)
 
@@ -24,13 +24,20 @@ class resNet(nn.Module):
         self.x3conv1024to512 = nn.Conv2d(1024,512,3,1,1,bias=False)
         self.x3conv1024to1024 = nn.Conv2d(1024,1024,3,1,1,bias=False)
 
+        self.normal64 = nn.BatchNorm2d(64)
+        self.normal128 = nn.BatchNorm2d(128)
+        self.normal256 = nn.BatchNorm2d(256)
+        self.normal512 = nn.BatchNorm2d(512)
+        self.normal1024 = nn.BatchNorm2d(1024)
+        self.d1normal1024 = nn.BatchNorm1d(1024)
+
         self.relu = nn.ReLU(inplace=True)
 
         self.pool = nn.MaxPool2d(2)
-        self.fc_pool = nn.AvgPool2d(14, stride=1)
+        self.fc_pool = nn.AvgPool2d(int(img_width/16), stride=1)
 
         self.linear1024to1024 = nn.Linear(1024, 1024)
-        self.linear1024to2 = nn.Linear(1024, 2)
+        self.linear1024to2 = nn.Linear(1024, 3)
         self.drop = nn.Dropout(0.1)
 
         # torch.cat([x, y], dim=1)
@@ -40,45 +47,54 @@ class resNet(nn.Module):
     def forward(self, x):
         output = self.x7conv3to64(x)
         output = self.pool(output)
-        print(output.shape)
+        # print('1',output.shape)
         res = self.x3conv64to64(output)
         res = self.x1conv64to64(res)
-        print(output.shape, res.shape)
+        # print('2',output.shape, res.shape)
+        output = self.normal64(output)
         output = self.relu(self.x3conv64to64(output))
         output = self.relu(self.x3conv64to64(output))
         
         output = torch.cat([output, res], dim=1)
         res = self.x1conv128to64(output)
+        
         output = self.relu(self.x3conv128to64(output))
+        output = self.normal64(output)
         output = self.relu(self.x3conv64to64(output))
 
         output = torch.cat([output, res], dim=1)
         res = self.x1conv128to64(output)
+        
         output = self.relu(self.x3conv128to64(output))
+        output = self.normal64(output)
         output = self.relu(self.x3conv64to64(output))
 
         output = torch.cat([output, res], dim=1)  # 1
         output = self.x3conv128to128(output)
         output = self.pool(output)
         res = output
+        output = self.normal128(output)
         output = self.relu(self.x3conv128to128(output))
         output = self.relu(self.x3conv128to128(output))
 
         output = torch.cat([output, res], dim=1) # 2
         output = self.x3conv256to128(output)
         res = output
+        output = self.normal128(output)
         output = self.relu(self.x3conv128to128(output))
         output = self.relu(self.x3conv128to128(output))
 
         output = torch.cat([output, res], dim=1)# 3
         output = self.x3conv256to128(output)
         res = output
+        output = self.normal128(output)
         output = self.relu(self.x3conv128to128(output))
         output = self.relu(self.x3conv128to128(output))
 
         output = torch.cat([output, res], dim=1)# 4
         output = self.x3conv256to128(output)
         res = output
+        output = self.normal128(output)
         output = self.relu(self.x3conv128to128(output))
         output = self.relu(self.x3conv128to128(output))
 
@@ -86,70 +102,82 @@ class resNet(nn.Module):
         output = self.x3conv256to256(output)
         output = self.pool(output)
         res = output
+        output = self.normal256(output)
         output = self.relu(self.x3conv256to256(output))
         output = self.relu(self.x3conv256to256(output))
         
         output = torch.cat([output, res], dim=1)
         output = self.x3conv512to256(output)
         res = output
+        output = self.normal256(output)
         output = self.relu(self.x3conv256to256(output))
         output = self.relu(self.x3conv256to256(output))
 
         output = torch.cat([output, res], dim=1)
         output = self.x3conv512to256(output)
         res = output
+        output = self.normal256(output)
         output = self.relu(self.x3conv256to256(output))
         output = self.relu(self.x3conv256to256(output))
 
         output = torch.cat([output, res], dim=1)
         output = self.x3conv512to256(output)
         res = output
+        output = self.normal256(output)
         output = self.relu(self.x3conv256to256(output))
         output = self.relu(self.x3conv256to256(output))
 
         output = torch.cat([output, res], dim=1)
         output = self.x3conv512to256(output)
         res = output
+        output = self.normal256(output)
         output = self.relu(self.x3conv256to256(output))
         output = self.relu(self.x3conv256to256(output))
 
         output = torch.cat([output, res], dim=1)
         output = self.x3conv512to256(output)
         res = output
+        output = self.normal256(output)
         output = self.relu(self.x3conv256to256(output))
         output = self.relu(self.x3conv256to256(output))
 
         output = torch.cat([output, res], dim=1)
         output = self.x3conv512to512(output)
         res = output
+        output = self.normal512(output)
         output = self.relu(self.x3conv512to512(output))
         output = self.relu(self.x3conv512to512(output))
 
         output = torch.cat([output, res], dim=1)
         output = self.x3conv1024to512(output)
         res = output
+        output = self.normal512(output)
         output = self.relu(self.x3conv512to512(output))
         output = self.relu(self.x3conv512to512(output))
 
         output = torch.cat([output, res], dim=1)
         output = self.x3conv1024to512(output)
         res = output
+        output = self.normal512(output)
         output = self.relu(self.x3conv512to512(output))
         output = self.relu(self.x3conv512to512(output))
 
         output = torch.cat([output, res], dim=1)
         output = self.x3conv1024to1024(output)
         output = self.pool(output)
-        print('4.4', output.shape)
+        # print('4.4', output.shape)
         output = self.fc_pool(output)
-        print('4.5', output.shape)
+        # print('4.5', output.shape)
         output = output.reshape(output.shape[0], -1)
-        print('5', output.shape)
+        output = self.d1normal1024(output)
+        # print('5', output.shape)
         output = self.drop(self.linear1024to1024(output))
+        output = self.d1normal1024(output)
         output = self.relu(self.linear1024to1024(output))
         output = self.drop(self.linear1024to1024(output))
+        output = self.d1normal1024(output)
         output = self.relu(self.linear1024to2(output))
-        print('6', output.shape)
+        # print('6', output.shape)
         return output
         
 import cv2 as cv 
@@ -179,15 +207,24 @@ class data_classif(Dataset):
             print("data error")
 
         image = TF.to_tensor(image)
-        target = TF.to_tensor(target)
 
         resize = transforms.Resize(size=self.input_size,antialias=True)
         image = resize(image)
         return image, target
 
+def dice_coefficient(preds, targets):
+    smooth = 1.0
+    assert preds.size() == targets.size()
+
+    iflat = preds.contiguous().view(-1)
+    tflat = targets.contiguous().view(-1)
+    intersection = (iflat * tflat).sum()
+    dice = (2.0 * intersection + smooth) / (iflat.sum() + tflat.sum() + smooth)
+    return dice
+
 def CNN_train(
-    epoch=20,
-    batch=20
+    epoch=3,
+    batch=5
 ):
     import os
     import pandas as pd
@@ -218,9 +255,38 @@ def CNN_train(
     dataset_df = pd.concat([benign_df,malignant_df,normal_df])
 
     device = 'cuda'if torch.cuda.is_available() else 'cpu'
-    model = resNet(1,3).to(device=device)
+    model = resNet(1,1).to(device=device)
+    data = data_classif(dataset_df)
 
-    data = data_classif(dataset_df, )
+    from torch.utils.data import random_split
+    train_data, test_data = random_split(data, [int(len(data)*0.9), len(data)-int(len(data)*0.9)])
+    
+    from torch.utils.data import DataLoader
+    train_deter_Loader = DataLoader(train_data, batch, shuffle=True, drop_last=True)
+    test_deter_Loader = DataLoader(test_data, batch, shuffle=False, drop_last=True)
 
-CNN_train()
+    import torch.optim as optim
+    optimizer = optim.Adam(model.parameters(), 0.001)
+    loss_f = nn.CrossEntropyLoss() if model.out_channel > 1 else nn.BCEWithLogitsLoss()
+
+    for e in range(1, epoch):
+        epoch_loss = 0
+        # from tqdm import tqdm
+        for idx, (predictor, target) in enumerate(train_deter_Loader):
+            predictor = predictor.to(device, torch.float32)
+            target = target.to(device, torch.float32)
+            prediction = model(predictor)
+            loss = loss_f(prediction, target)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            print(f"\t\tBatch {idx+1} done, with loss = {loss}")
+        print(f"[+] epoch {epoch+1} done, with loss = {epoch_loss/len(train_deter_Loader)}")
+    return model
+
+
+model = CNN_train()
+
+
 
